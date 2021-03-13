94bb3ea3fe44117607644f48c2d73266a95d5b49
Rodrigo <rodrigo.hammes7@gmail.com>
2021-03-13 15:39:00 -0300

    primeiro componente react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './styles/global.scss';
+import { RepositoryList } from './components/RepositoryList';
 
 export function App(){
 
-  return <h1>Hello Batata</h1>
+  return <RepositoryList />
 }
---
+++ src/components/RepositoryList/index.jsx
@@ -0,0 +1,36 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
