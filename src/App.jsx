795b540cc19940e6e03653769933006a02b122be
Rodrigo <rodrigo.hammes7@gmail.com>
2021-04-15 22:22:02 -0300

    estilizaçao da listagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './styles/global.scss';
 import { RepositoryList } from './components/RepositoryList';
-import { Counter } from './components/Counter';
 
 export function App(){
 
   return (
     <>
       <RepositoryList />
-      <Counter />
     </>
   )
 }
